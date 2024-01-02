dbed0b7f4800b36c9d46ced05d79771c03b29b96
AhmadAbdElhameed <Abdelhameed9@yahoo.com>
2024-01-02 10:58:48 +0200

    adding languages to sidebar

---
+++ lang/en/admin/sidebar.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+   'settings' => 'Settings',
+   'shipping_rules' => 'Shipping Rules',
+   'free_shipping' => 'Free Shipping',
+   'inner_shipping' => 'Inner Shipping',
+   'outer_shipping' => 'Outer Shipping',
+];
