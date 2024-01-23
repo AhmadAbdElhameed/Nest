1fa9d4ed255f0e31b9e85a927ac32881fc11d505
AhmadAbdElhameed <Abdelhameed9@yahoo.com>
2024-01-23 18:31:26 +0200

    product attributes

---
+++ app/Models/Attribute.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attribute extends Model
+{
+    use HasFactory,Translatable;
+
+    protected $guarded = [];
+
+    protected $with = ['translations'];
+    protected $translatedAttributes = ['name'];
+
+}
