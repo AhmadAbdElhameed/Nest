b5ef910e44c62b2bda96c49b2584814e9093da94
AhmadAbdElhameed <Abdelhameed9@yahoo.com>
2024-01-27 19:39:26 +0200

    enable email verification and notification

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Notifications\EmailVerification;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\URL;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -42,4 +45,15 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function sendEmailVerificationNotification(){
+        $url = URL::temporarySignedRoute(
+            'verification.verify', now()->addMinutes(30), [
+                'id' => $this->getKey(),
+                'hash' => sha1($this->getEmailForVerification())
+            ]
+        );
+
+        $this->notify(new EmailVerification($url));
+    }
 }
