417f5d13c01fb76d498ceed50bceeaf5ceaa2bda
AhmadAbdElhameed <Abdelhameed9@yahoo.com>
2024-02-04 10:10:04 +0200

    cash on delivery payment

--- app/Http/Controllers/Front/CheckoutController.php
+++ app/Http/Controllers/Front/CheckoutController.php
@@ -81,7 +81,7 @@ class CheckoutController extends Controller
         if($request->payment_method === 'stripe'){
             return $this->stripePayment($order_details['total']);
         }elseif ($request->payment_method === 'cod'){
-            return $this->codPayment($request);
+            return $this->codPayment();
         }else{
             toast()->error('Something went wrong please try again later!');
             return redirect()->route('shop');
@@ -151,5 +151,36 @@ class CheckoutController extends Controller
     {
         toast()->error('Payment failed, Please try again.');
         return redirect()->route('checkout.index');
+    }
+
+    private function codPayment(){
+        $checkout_details = Session::get('checkout_details');
+        $order = Order::create([
+            'payment_method' => 'cod',
+            'phone' => $checkout_details['phone'],
+            'user_id' =>$checkout_details['user_id'],
+            'name' => $checkout_details['name'],
+            'email' => $checkout_details['email'],
+            'city' => $checkout_details['city'],
+            'address' => $checkout_details['address'],
+            'notes' => $checkout_details['notes'] ?? NULL,
+            'payment_status' => 'pending',
+            'total_amount' => $checkout_details['total']
+        ]);
+        $cartProducts = $checkout_details['cart_items'];
+        foreach ($cartProducts as $product){
+            OrderItem::create([
+                'order_id' => $order->id,
+                'product_id' => $product['id'],
+                'quantity' => $product['qty'],
+                'price' => $product['price'],
+            ]);
+        }
+
+//        OrderPlacedNotificationEvent::dispatch($order);
+        Cart::destroy();
+
+        toast()->success('Order confirmed & our team will call you soon.');
+        return redirect()->route('home');
     }
 }
