e8dbad324a315e69d3a49636e5af8465807985aa
AhmadAbdElhameed <Abdelhameed9@yahoo.com>
2024-02-07 11:18:44 +0200

    store admin method with validation

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Admins\StoreAdminRequest;
 use App\Models\Admin;
+use App\Models\Role;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -12,5 +16,35 @@ class UserController extends Controller
         $users = Admin::latest()->where('id','<>',auth()->id())->get();
 
         return view('admin.users.index',compact('users'));
+    }
+
+    public function create(){
+        $roles = Role::get();
+
+        return view('admin.users.create',compact('roles'));
+    }
+
+    public function store(StoreAdminRequest $request)
+    {
+        // Begin a transaction in case part of the operation fails
+        DB::beginTransaction();
+        try {
+            $admin = Admin::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password), // Ensure the password is hashed
+                'role_id' => $request->role_id, // Associate the admin with a role
+            ]);
+
+            DB::commit(); // Commit the transaction if no errors
+            toast('Admin created successfully','success');
+            return redirect()->route('admin.user.index')->with('success', 'Admin created successfully.');
+
+        } catch (\Exception $e) {
+            DB::rollBack(); // Rollback the transaction on error
+            toast('Failed to create admin. Please try again','error');
+            // Redirect back with an error message
+            return back()->withInput()->withErrors(['error' => 'Failed to create admin. Please try again.']);
+        }
     }
 }
